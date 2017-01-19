1ad9c9bccfe18d6d8af568f019f06fd2c62c4859
Slavyan <slavyan.mladenov@gmail.com>
2017-01-19 11:29:07 -0800

    +VrScene+VrScript

---
+++ Assets/Flying.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flying : MonoBehaviour {
+
+	public Transform VrCamera;
+
+	public int ToggleAngle = 90;
+
+	public float flyspeed = 3.0f;
+
+	private bool fly;
+
+	private CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController> ();
+	}
+	void SetTransformX(float n){
+		transform.position = new Vector3(transform.position.x,n,transform.position.z);
+	}
+	// Update is called once per frame
+	void Update () {
+		//if (VrCamera.eulerAngles.x <= ToggleAngle)
+		if (VrCamera.eulerAngles.x >=ToggleAngle)	
+		fly = true;
+		else {
+			fly = false;
+		}
+
+		if (fly) {
+
+			SetTransformX (flyspeed);
+			flyspeed++;
+		}
+
+	}
+}
---
+++ Assets/SetPar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPar : MonoBehaviour {
+
+	private GameObject CameraRight;
+	private GameObject CameraLeft;
+
+
+
+	public Transform Parent;
+	// Use this for initialization
+	// Update is called once per frame
+	void Update()
+	{
+			CameraLeft = GameObject.Find ("Main Camera Left");
+			CameraRight = GameObject.Find ("Main Camera Right");
+			CameraLeft.transform.SetParent (Parent);
+			CameraRight.transform.SetParent (Parent);
+
+
+	}
+
+
+
+
+
+}
