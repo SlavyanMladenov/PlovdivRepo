751420308a37f909d2be451d0b5dab9d4f374ca5
Slavyan <slavyan.mladenov@gmail.com>
2017-01-21 04:42:12 -0800

    +mic

---
+++ Assets/Scripts/MicrophoneInput.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInput : MonoBehaviour
+{
+    public float sensitivity = 100.0f;
+    public float loudness = 0.0f;
+    public float frequency = 0.0f;
+    public int samplerate = 11024;
+    string AudioInputDevice = "";
+    public AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 10, samplerate);
+        audio.loop = true; // Set the AudioClip to loop
+        audio.mute = false; // Mute the sound, we don't want the player to hear it
+        while (!(Microphone.GetPosition(AudioInputDevice) > 0)) { } // Wait until the recording has started
+        audio.Play(); // Play the audio source!
+    }
+
+    void Update()
+    {
+        loudness = GetAveragedVolume() * sensitivity;
+        frequency = GetFundamentalFrequency();
+    }
+
+    float GetAveragedVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        audio.GetOutputData(data, 0);
+        foreach (float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+        return a / 256;
+    }
+
+    float GetFundamentalFrequency()
+    {
+        float fundamentalFrequency = 0.0f;
+        float[] data = new float[8192];
+        audio.GetSpectrumData(data, 0, FFTWindow.BlackmanHarris);
+        float s = 0.0f;
+        int i = 0;
+        for (int j = 1; j < 8192; j++)
+        {
+            if (s < data[j])
+            {
+                s = data[j];
+                i = j;
+            }
+        }
+        fundamentalFrequency = i * samplerate / 8192;
+        return fundamentalFrequency;
+	
+    }
+}
---
+++ Assets/Scripts/NoteFinder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NoteFinder : MonoBehaviour
+{
+    public GameObject audioInputObject;
+
+    public float threshold = 1.0f;
+    MicrophoneInput micIn;
+	public AudioSource audio;
+    // Use this for initialization
+    void Start()
+    {
+       
+        micIn = (MicrophoneInput)audioInputObject.GetComponent("MicrophoneInput");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float t = micIn.loudness;//Get the loudness from our MicrophoneInput script
+		int f = (int)micIn.frequency;// Get the frequency from our MicrophoneInput script
+		if (t>=0.8) // Compare the frequency to known value, take possible rounding error in to account
+        {
+			audio.Play();
+
+
+            Debug.Log("WORKING!");
+        }
+        
+    }
+}
