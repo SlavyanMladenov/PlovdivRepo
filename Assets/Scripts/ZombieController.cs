936a1deec7c1a0dc98c92c6ee50bba21e0bb9bc9
Ned <nedvnedyalkov@gmail.com>
2017-01-21 16:23:14 +0200

    temp

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+	private bool playerInRange = false;
+	private Animator animator;
+
+	void Awake()
+	{
+		animator = GetComponent<Animator> ();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Player"))
+		{
+			animator.SetBool ("Attack", true);
+		}
+	}
+}
